dca798613de60d9b08b54a0e031ea300be28ef41
YiTong.Zhang <zyt@YiTongZhangdeMacBook-Pro.local>
2014-11-01 12:48:36 +0800

    修改了setting

--- weather/settingInfo/SettingInfoViewController.m
+++ weather/settingInfo/SettingInfoViewController.m
@@ -38,7 +38,7 @@
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-        // Custom initialization
+        //  initialization
     }
     return self;
 }
